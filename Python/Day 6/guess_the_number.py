ae8ab1b3ab24725dab44c0241c84a5f224987474
Adeeb Imam <adeebimam2511@gmail.com>
2024-05-27 14:34:34 +0100

    updated guess_the_number file

--- Python/Day 6/guess_the_number.py
+++ Python/Day 6/guess_the_number.py
@@ -1,42 +1,38 @@
-"""
-In this program we will learn a new topic known as "while loop" and based on our understanding
-of this topic we will build a program that would select a random number and then ask the user 
-to guess that number, the user will have only 5 attempts to guess the number
-
-A while loop is a programming construct that repeatedly executes a block of code 
-as long as a certain condition remains true.
-
-Done By Adeeb Imam
-Date 23rd May 2024
-
-"""
-
-#Importing and initialzing all the necessary variables for the program
-
+# Importing the random library to generate random numbers.
 import random
 
+# Initializing the maximum number of guesses allowed.
 max_guess = 5 
+
+# Generating a random number between 1 and 100 to be guessed.
 guess_num = random.randint(1, 100)
 
+# Greeting the user and introducing the game.
 print("Welcome to the guessing game!")
 
+# Initialize game status to not won and set the trial count to zero.
 game_won = False
 trial = 0
 
+# Start a while loop that runs as long as the game has not been won.
 while not game_won:
-    user_guess_num = int(input("Please enter the number: "))  # Prompt moved inside the loop
+    # Prompt the user to enter a guess and convert the input to an integer.
+    user_guess_num = int(input("Please enter the number: "))  
     
+    # Check if the user's guess is equal to the randomly generated number.
     if user_guess_num == guess_num:
+        # Inform the user of winning and update game status to won.
         print("You have won the game!")
         game_won = True
     else:
+        # Inform the user that the guess was incorrect.
         print("Incorrect answer, please try again")
+        
+        # Increment the trial count because the guess was wrong.
         trial += 1
+        
+        # Check if the number of trials has reached the maximum allowed guesses.
         if trial >= max_guess:
+            # Inform the user that they've run out of guesses and end the game.
             print("You've used all your guesses. Game over!")
-            break
-
-
-
-
-
+            break  # Exit the loop since the game is over either by winning or exceeding trials.
